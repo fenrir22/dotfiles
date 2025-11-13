03196eb363921d1aad590819cafafc6b263604d4
fenrir22 <68318422+fenrir22@users.noreply.github.com>
2025-11-13 13:13:27 +0100

    Update setup.sh

--- setup.sh
+++ setup.sh
@@ -3,7 +3,7 @@
 set -e
 
 DOTFILES_DIR="$HOME/.local/share/dotfiles"
-REPO_URL="https://github.com/Maciejonos/dotfiles.git"
+REPO_URL="https://github.com/fenrir22/dotfiles.git"
 
 echo "=============="
 echo "Dotfiles Setup"
